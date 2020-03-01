d84d0623f3bd742d6d187ae08a0137fbe346ad91
lmh <lmh@local>
2020-03-01 17:53:13 +0100

    added install script with git functions

---
+++ README.md
@@ -0,0 +1,18 @@
+# ClamavBox
+
+
+***
+
+## Prerequisites
+* A Raspberry Pi
+* SD-Card
+* Power-Source
+* A fresh installation of [Raspbian](https://www.raspberrypi.org/downloads/raspbian/)  is recommended.
+
+## Installation
+
+
+## Acknowledgements
+
+Thanks to ...
+* ... the guys from [pi-hole](https://github.com/pi-hole/pi-hole/blob/master/automated%20install/basic-install.sh) for their inspiration with the setup-script
---
+++ install.sh
@@ -0,0 +1,208 @@
+#!/usr/bin/env bash
+# shellcheck disable=SC1090
+
+# -e option instructs bash to immediately exit if any command [1] has a non-zero exit status
+# We do not want users to end up with a partially working install, so we exit the script
+# instead of continuing the installation with something broken
+set -e
+
+AVBOX_REPO_DIR="/home/pi/avbox"
+AVBOX_GIT_URL="https://git.hilberg.ddnss.de/lmh/clamavBox.git"
+
+# Set these values so the installer can still run in color
+COL_NC='\e[0m' # No Color
+COL_LIGHT_GREEN='\e[1;32m'
+COL_LIGHT_RED='\e[1;31m'
+TICK="[${COL_LIGHT_GREEN}✓${COL_NC}]"
+CROSS="[${COL_LIGHT_RED}✗${COL_NC}]"
+INFO="[i]"
+# shellcheck disable=SC2034
+DONE="${COL_LIGHT_GREEN} done!${COL_NC}"
+OVER="\\r\\033[K"
+
+# A function for checking if a directory is a git repository
+is_repo() {
+    # Use a named, local variable instead of the vague $1, which is the first argument passed to this function
+    # These local variables should always be lowercase
+    local directory="${1}"
+    # A local variable for the current directory
+    local curdir
+    # A variable to store the return code
+    local rc
+    # Assign the current directory variable by using pwd
+    curdir="${PWD}"
+    # If the first argument passed to this function is a directory,
+    if [[ -d "${directory}" ]]; then
+        # move into the directory
+        cd "${directory}"
+        # Use git to check if the directory is a repo
+        # git -C is not used here to support git versions older than 1.8.4
+        git status --short &> /dev/null || rc=$?
+    # If the command was not successful,
+    else
+        # Set a non-zero return code if directory does not exist
+        rc=1
+    fi
+    # Move back into the directory the user started in
+    cd "${curdir}"
+    # Return the code; if one is not set, return 0
+    return "${rc:-0}"
+}
+
+# A function to clone a repo
+make_repo() {
+    # Set named variables for better readability
+    local directory="${1}"
+    local remoteRepo="${2}"
+    # The message to display when this function is running
+    str="Clone ${remoteRepo} into ${directory}"
+    # Display the message and use the color table to preface the message with an "info" indicator
+    printf "  %b %s..." "${INFO}" "${str}"
+    # If the directory exists,
+    if [[ -d "${directory}" ]]; then
+        # delete everything in it so git can clone into it
+        rm -rf "${directory}"
+    fi
+    # Clone the repo and return the return code from this command
+    git clone -q --depth 20 "${remoteRepo}" "${directory}" &> /dev/null || return $?
+    # Show a colored message showing it's status
+    printf "%b  %b %s\\n" "${OVER}" "${TICK}" "${str}"
+    # Always return 0? Not sure this is correct
+    return 0
+}
+
+# We need to make sure the repos are up-to-date so we can effectively install Clean out the directory if it exists for git to clone into
+update_repo() {
+    # Use named, local variables
+    # As you can see, these are the same variable names used in the last function,
+    # but since they are local, their scope does not go beyond this function
+    # This helps prevent the wrong value from being assigned if you were to set the variable as a GLOBAL one
+    local directory="${1}"
+    local curdir
+
+    # A variable to store the message we want to display;
+    # Again, it's useful to store these in variables in case we need to reuse or change the message;
+    # we only need to make one change here
+    local str="Update repo in ${1}"
+
+    # Make sure we know what directory we are in so we can move back into it
+    curdir="${PWD}"
+    # Move into the directory that was passed as an argument
+    cd "${directory}" &> /dev/null || return 1
+    # Let the user know what's happening
+    printf "  %b %s..." "${INFO}" "${str}"
+    # Stash any local commits as they conflict with our working code
+    git stash --all --quiet &> /dev/null || true # Okay for stash failure
+    git clean --quiet --force -d || true # Okay for already clean directory
+    # Pull the latest commits
+    git pull --quiet &> /dev/null || return $?
+    # Show a completion message
+    printf "%b  %b %s\\n" "${OVER}" "${TICK}" "${str}"
+    # Move back into the original directory
+    cd "${curdir}" &> /dev/null || return 1
+    return 0
+}
+
+# A function that combines the functions previously made
+getGitFiles() {
+    # Setup named variables for the git repos
+    # We need the directory
+    local directory="${1}"
+    # as well as the repo URL
+    local remoteRepo="${2}"
+    # A local variable containing the message to be displayed
+    local str="Check for existing repository in ${1}"
+    # Show the message
+    printf "  %b %s..." "${INFO}" "${str}"
+    # Check if the directory is a repository
+    if is_repo "${directory}"; then
+        # Show that we're checking it
+        printf "%b  %b %s\\n" "${OVER}" "${TICK}" "${str}"
+        # Update the repo, returning an error message on failure
+        update_repo "${directory}" || { printf "\\n  %b: Could not update local repository. Contact support.%b\\n" "${COL_LIGHT_RED}" "${COL_NC}"; exit 1; }
+    # If it's not a .git repo,
+    else
+        # Show an error
+        printf "%b  %b %s\\n" "${OVER}" "${CROSS}" "${str}"
+        # Attempt to make the repository, showing an error on failure
+        make_repo "${directory}" "${remoteRepo}" || { printf "\\n  %bError: Could not update local repository. Contact support.%b\\n" "${COL_LIGHT_RED}" "${COL_NC}"; exit 1; }
+    fi
+    # echo a blank line
+    echo ""
+    # and return success?
+    return 0
+}
+
+# Reset a repo to get rid of any local changed
+resetRepo() {
+    # Use named variables for arguments
+    local directory="${1}"
+    # Move into the directory
+    cd "${directory}" &> /dev/null || return 1
+    # Store the message in a variable
+    str="Resetting repository within ${1}..."
+    # Show the message
+    printf "  %b %s..." "${INFO}" "${str}"
+    # Use git to remove the local changes
+    git reset --hard &> /dev/null || return $?
+    # And show the status
+    printf "%b  %b %s\\n" "${OVER}" "${TICK}" "${str}"
+    # Returning success anyway?
+    return 0
+}
+
+clone_or_update_repos() {
+        getGitFiles ${AVBOX_REPO_DIR} ${AVBOX_GIT_URL} || \
+        { printf "  %bUnable to clone %s into %s, unable to continue%b\\n" "${AVBOX_GIT_URL}" "${AVBOX_REPO_DIR}"; \
+        exit 1; \
+}
+
+# Enable service so that it will start with next reboot
+enable_service() {
+    # Local, named variables
+    local str="Enabling ${1} service to start on reboot"
+    printf "  %b %s..." "${INFO}" "${str}"
+    # If systemctl exists,
+    if is_command systemctl ; then
+        # use that to enable the service
+        systemctl enable "${1}" &> /dev/null
+    # Otherwise,
+    else
+        # use update-rc.d to accomplish this
+        update-rc.d "${1}" defaults &> /dev/null
+    fi
+    printf "%b  %b %s...\\n" "${OVER}" "${TICK}" "${str}"
+}
+
+# Disable service so that it will not with next reboot
+disable_service() {
+    # Local, named variables
+    local str="Disabling ${1} service"
+    printf "  %b %s..." "${INFO}" "${str}"
+    # If systemctl exists,
+    if is_command systemctl ; then
+        # use that to disable the service
+        systemctl disable "${1}" &> /dev/null
+    # Otherwise,
+    else
+        # use update-rc.d to accomplish this
+        update-rc.d "${1}" disable &> /dev/null
+    fi
+    printf "%b  %b %s...\\n" "${OVER}" "${TICK}" "${str}"
+}
+
+check_service_active() {
+    # If systemctl exists,
+    if is_command systemctl ; then
+        # use that to check the status of the service
+        systemctl is-enabled "${1}" &> /dev/null
+    # Otherwise,
+    else
+        # fall back to service command
+        service "${1}" status &> /dev/null
+    fi
+}
+
+
+# Clone/Update the repos
+clone_or_update_repos
